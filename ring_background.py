8835da6a37d204d4b183a60b56a63d05de720811
Atreyee SINHA <asinha@sinfos-MacBook-Pro.local>
2020-11-20 13:59:39 +0100

    adaptive ring

--- ring_background.py
+++ ring_background.py
@@ -1,4 +1,4 @@
-from gammapy.makers import RingBackgroundMaker
+from gammapy.makers import RingBackgroundMaker, AdaptiveRingBackgroundMaker
 from gammapy.estimators import ExcessMapEstimator
 from gammapy.maps import Map
 from gammapy.datasets import MapDatasetOnOff
@@ -48,5 +48,29 @@ def extract_ring(datasets,
     return stacked_on_off
 
 
+def extract_ring_adaptive(datasets,
+                 exclusion_mask,
+                 r_in="0.6 deg",
+                 width="0.3 deg",
+                 r_out_max=2.3*u.deg,
+                 method = 'fixed_width'):
+    geom = datasets[0].counts.geom
+    energy_axis = datasets[0].counts.geom.axes["energy"]
+    geom_image = geom.to_image().to_cube([energy_axis.squash()])
 
+    ring_maker = AdaptiveRingBackgroundMaker(
+        r_in=r_in, width=width, exclusion_mask=exclusion_mask,
+        r_out_max = r_out_max, method=method
+    )
+
+    energy_axis_true = datasets[0].exposure.geom.axes["energy_true"]
+    stacked_on_off = MapDatasetOnOff.create(
+        geom=geom_image, energy_axis_true=energy_axis_true, name="stacked"
+    )
+
+    for dataset in datasets:
+        dataset_on_off = ring_maker.run(dataset.to_image())
+        stacked_on_off.stack(dataset_on_off)
+
+    return stacked_on_off
 
